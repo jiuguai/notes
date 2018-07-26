cb0de16a477802123794d11de61dcb83fc396061
jiuguai <jijiyezero@163.com>
2018-07-26 22:43:51 +0700

    first_c

---
+++ 数据分析/常.py
@@ -0,0 +1,86 @@
+count
+
+
+sum
+cunsum
+avg
+quantile
+mode	.tolist
+
+
+max = min 
+std
+var
+
+# 异常
+
+[avg - 3*std,avg + 3*std]
+
+qmin = quantile(q=0.25)
+qmax = quantile(q=0.75)
+iqr = qmax - qmin
+[qmin - 1.5*iqr,qmax + 1.5*iqr]
+
+
+
+
+# 分布
+值分布
+值域分布
+
+按照某字段分组
+
+# 对比
+结构对比
+比例对比
+空间对比
+动态对比
+
+
+# 统计分析
+集中程度
+avg
+quantile(q=0.5)
+mode	.tolist
+
+分散程度
+max - min
+std
+var
+
+
+# 28定律
+80% 的贡献 来自于 20%的产出
+80% 的资源掌握在 20%人手里
+80% 的权利掌握在 20%人手里
+起到重要价值的 在结构中少数部分
+90% 的思虑只是 为那 10%总结做铺垫
+95% 行为	只是 5% 思想所指引
+
+# 正态分布
+图判
+
+对值定性分析
+
+
+# 相关性分析
+图判
+Pearson相关系数
+Sperman秩相关系数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
