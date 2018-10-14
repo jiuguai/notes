2d688ddc86bfcc57baa1f42e24578f9472a0c595
jiuguai <jijiyezero@163.com>
2018-10-15 01:15:02 +0800

    modify crawle.md

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-function fib(n,a = 0,b = 1){
+// function fib(n,a = 0,b = 1){
 
-  if(n === 1){
-    return a
-  }
+//   if(n === 1){
+//     return a
+//   }
 
-  return fib(n - 1 , b,a + b)
+//   return fib(n - 1 , b,a + b)
 
-}
+// }
 
-console.log(fib(1))
+// console.log(fib(100))
+console.log(55)
