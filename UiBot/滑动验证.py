ee5099b840226fa5ca1705385c4a9f382a58305e
zero <wind2zero@qq.com>
2020-06-14 20:55:48 +0800

    2020年6月14日

--- UiBot/滑动验证.py
+++ UiBot/滑动验证.py
@@ -5,24 +5,28 @@ import cv2 as cv
 bg = cv.imread()
 front = cv.imread()
 
+# 灰度化
 bg = cv.cvtColor(bg, cv.COLOR_BGR2GRAY)
 
 front = cv.cvtColor(front, cv.COLOR_BGR2GRAY)
 
-#  处理滑块
+#  逐行处理 每行有记录的时候就保留，否则社区
 front = front[front.any(1)]
 
 
-# 匹配
+# 匹配图像
 result = cv.matchTemplate(bg, front, cv.TM_CCOEFF_NORMED)
 
-np.argmax(result)
+#  一维数组
+arr = np.argmax(result)
 
-x, y = np.unravel_index(np.argmax(result),result.shape)
+# 更换为二维数组
+x, y = np.unravel_index(arr,result.shape)
 
 # 测试
 
 w, h = front.shape
+# 画框
 cv.rectangle(bg, (y,x), (y+w, x+h), (38,38,38),2)
 
 
